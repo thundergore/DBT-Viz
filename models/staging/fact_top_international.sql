1cb6f645c514b690d6ac1f729a09a931eb8109fb
cturner573@gmail.com <cturner573@gmail.com>
2022-11-09 09:42:24 +0000

    remove reference to region_code and replace with region_id

--- models/staging/fact_top_international.sql
+++ models/staging/fact_top_international.sql
@@ -2,7 +2,7 @@ select   week
        , country_name
        , country_code
        , region_name
-       , region_code
+       , region_code as region_id
        , term as search_term
        , rank
        , score 
