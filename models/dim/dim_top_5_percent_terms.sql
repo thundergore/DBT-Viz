904d99e1de11ef666202da3edfed03712b71120a
cturner573@gmail.com <cturner573@gmail.com>
2022-11-11 10:34:19 +0000

    Fixing issue #9

--- models/dim/dim_top_5_percent_terms.sql
+++ models/dim/dim_top_5_percent_terms.sql
@@ -1,7 +1,9 @@
 select  distinct date_week 
       , country_code
       , search_term
+      , rank
       , ntile(20) over (partition by country_name order by rank asc) AS ntile_rank
 from {{ ref('dim_top_this_year_merged') }}
-qualify ntile_rank >= 5
+where rank >= 5 --unsure if this is the logic tweak needed, let's test
+qualify ntile_rank <= 5
 order by date_week desc, country_code asc, ntile_rank asc
